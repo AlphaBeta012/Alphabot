e5bc0082fb6e48f2b2ad2939ccfb94d886b9543c
AlphaBeta012 <88704081+AlphaBeta012@users.noreply.github.com>
2021-08-10 13:20:59 +0700

    Add files via upload

---
+++ Alphabot.py
@@ -0,0 +1,23 @@
+import discord
+from discord.ext import commands
+import os
+import random
+
+client = commands.Bot(command_prefix = '-')
+
+@client.event
+async def on_ready():
+    print("bot is ready!")
+    
+@client.command()
+async def ping(ctx):
+    await ctx.send('pong!')
+
+@client.command()
+async def meme(ctx):
+        image = os.listdir('/storage/emulated/0/image_meme/')
+        imgString = random.choice(image)  
+        path = "/storage/emulated/0/image_meme/" + imgString
+        await ctx.send(file=discord.File(path))
+                                                        
+client.run('ODczMzQwNTAwNDM2OTc5NzQz.YQ2_uQ.eI4pitFZ4mUiEMoJI1mYUYwITY4')
